f806836d8d0c064e2b8d456eb6a77f3796adb824
iamGabby <gabiseef@gmail.com>
2020-08-25 18:51:27 -0500

    Linked list pattern twoPointers: cycle detection

---
+++ Algorithm/src/LinkedList/multiplePasses.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+/*
+ * Given a linked list, remove the nth node from the end of the list and return the head of the list. */
+public class multiplePasses {
+
+	// have two pointers 1. head 2. tail
+	// iterate over the list until the last element node.next == null, tail 
+		// check if the length of the linked list >= the |n|
+	// count down from the tail to head until the nth node from the end
+	// make (n-1)th node point to (n+1)th node
+	// return the head pointer
+}
